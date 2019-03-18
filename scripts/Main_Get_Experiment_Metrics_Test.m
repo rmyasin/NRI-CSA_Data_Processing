2fea54fa10e176a6fb5b4aa6ec0f0aa280d65945
Rashid <rashid.m.yasin@vanderbilt.edu>
2019-03-18 17:53:56 -0500

    Process test users 10 and 11

--- scripts/Main_Get_Experiment_Metrics_Test.m
+++ scripts/Main_Get_Experiment_Metrics_Test.m
@@ -1,25 +1,22 @@
 clear
 close all
-% clc
+clc
 
 % restoredefaultpath
 % addpath(genpath(getenv('ARMA_CL')))
 % addpath(genpath('Utilities'))
 
-% dataFolder='R:\Projects\NRI\User_Study\Data\user1_test_2019-02-13';
-% dataFolder='R:\Projects\NRI\User_Study\Data\user3_20190220_test';
-% dataFolder='R:\Projects\NRI\User_Study\Data\user3_20190220_test';
-% dataFolder='R:\Projects\NRI\User_Study\Data\user3_20190220_test';
+% TODO:
+% Update footpedals to take into account when the user adds/removes points
+% Add data saving struct to remember raw results and experiment metrics
+% Make a list of data Folders to be able to save and load data from subsets
+% of users
 
+dataFolder='R:\Projects\NRI\User_Study\Data\user11';
+
+plotOption=true;
 
 cpd_dir=getenv('CPDREG');
-% cpd_dir ='/home/arma/catkin_ws/src/cpd-registration';
-dataFolder='R:\Projects\NRI\User_Study\Data\user5';
-% dataFolder='/home/arma/catkin_ws/data/user5';
-dataFolder='R:\Projects\NRI\User_Study\Data\user6';
-dataFolder='R:\Projects\NRI\User_Study\Data\user7';
-
-
 featureFolder =[ cpd_dir filesep 'userstudy_data' filesep 'PLY'];
 organFolder=[cpd_dir filesep 'userstudy_data' filesep 'PointCloudData' filesep 'RegAprToCT'];
 
@@ -28,6 +25,11 @@ resultsStruct.userNumber=str2double(dataFolderName(5));
 contents=dir(dataFolder);
 regTimes=[];
 regNames={};
+expOrgan{5}={};
+expName{5}={};
+expOrgan{6}={};
+expName{6}={};
+
 for ii=1:length(contents)
     key = contents(ii).name;
     if startsWith(key,'Registration')
@@ -39,179 +41,239 @@ for ii=1:length(contents)
         finish = start+a(1)-2;
         expOrgan{1}= key(start:finish);
         expName{1}=contents(ii).name;
+    elseif startsWith(key,'Following_BarVision') && endsWith(key,'.txt')
+        start = length('Following_BarVision')+2;
+        a=strfind(key(start:end),'_');
+        finish = start+a(1)-2;
+        expOrgan{2}= key(start:finish);
+        expName{2}=contents(ii).name;
+        
     elseif startsWith(key,'Following_DirectForce') && endsWith(key,'.txt')
         start = length('Following_DirectForce')+2;
         a=strfind(key(start:end),'_');
         finish = start+a(1)-2;
-        expOrgan{2}= key(start:finish);
+        expOrgan{3}= key(start:finish);
+        expName{3}=contents(ii).name;
         
-        expName{2}=contents(ii).name;
     elseif startsWith(key,'Following_HybridForce') && endsWith(key,'.txt')
         start = length('Following_HybridForce')+2;
         a=strfind(key(start:end),'_');
         finish = start+a(1)-2;
-        expOrgan{3}= key(start:finish);
-        
-        expName{3}=contents(ii).name;
+        expOrgan{4}= key(start:finish);
+        expName{4}=contents(ii).name;
     elseif startsWith(key,'Palpation_DirectForce') && endsWith(key,'.txt')
         start = length('Palpation_DirectForce')+2;
         a=strfind(key(start:end),'_');
         finish = start+a(1)-2;
-        expOrgan{4}= key(start:finish);
+        expOrgan{5}= {expOrgan{5}{:},key(start:finish)};
+        expName{5}={expName{5}{:},contents(ii).name};
         
-        expName{4}=contents(ii).name;
     elseif startsWith(key,'Palpation_VisualForce') && endsWith(key,'.txt')
         start = length('Palpation_VisualForce')+2;
         a=strfind(key(start:end),'_');
         finish = start+a(1)-2;
-        expOrgan{5}= key(start:finish);
+        expOrgan{6}= {expOrgan{6}{:},key(start:finish)};
+        expName{6}={expName{6}{:},contents(ii).name};
         
-        expName{5}=contents(ii).name;
     end
 end
+
 % TODO: this needs an option for repeated experiments with different organs,
-% since palpation experiments will be repeated at least 1x per organ
+% since palpation experiments will be done 2x, one per each organ
 
-
-%%
-% Read in organ A, get ground truth location of organ/artery
-for ii=[]%2:3
+%% Process artery-following experiment
+for ii=[]%1:4
     organLabel=expOrgan{ii};
     
+    %% Read the robot data from txt file
     [output,vProtocol]=readRobTxt(dataFolder,expName{ii});
     cur=output.psm_cur;
     des=output.psm_des;
     force=output.force;
     forceTime = force.time;
-    
-    % Find when the robot is in contact
-    forceNorms=sqrt(sum(output.force.data.^2,2));
-    contactIndex=forceNorms>0.1;
-    contactChangeTimes=[];
-    if contactIndex(1)
-        contactChangeTimes=[contactChangeTimes;forceTime(1)];
-    end
-    contactChangeTimes=[contactChangeTimes;forceTime(find(diff(contactIndex)~=0))];
-    if contactIndex(end)
-        contactChangeTimes=[contactChangeTimes;forceTime(end)];
-    end
-    
-    curLeaveIndex=zeros(size(cur.time));
-    for jj=2:2:(length(contactChangeTimes)-1)
-        curLeaveIndex=curLeaveIndex | (cur.time>contactChangeTimes(jj) & cur.time<contactChangeTimes(jj+1));
-    end
-    if ~contactChangeTimes(end)
-        curLeaveIndex(end)=1;
-    end
-    
-    % Plot the in contact and free space data
-    vplot3(cur.pos(curLeaveIndex,:))
-    hold on
-    vplot3(cur.pos(~curLeaveIndex,:))
-    
-    % Plot the raw robot data
     micronTip=output.micronTip;
-    registrationIndex=find((cur.time(1)-regTimes)>0,1,'last'); %find the most recent registration
+    %% Get position data, find when the robot is in contact
+    
+    % Calculate force norm
+    forceNorms=sqrt(sum(output.force.data.^2,2));
+    
+    % Find positions when force above contact threshold by matching times
+    contactIndex=forceNorms>0.1;
+    contactTrimTimes=[];
+    if contactIndex(1)
+        contactTrimTimes=forceTime(1);
+    end
+    contactTrimTimes=[contactTrimTimes;forceTime(find(diff(contactIndex)~=0))];
+    if contactIndex(end)
+        contactTrimTimes=[contactTrimTimes;forceTime(end)];
+    end
+    
+    [curContact,curTimes]=trimBetweenTime(cur.pos/1000,cur.time,contactTrimTimes);
+    
+    %% Find the organ registration and the artery/organ points
+    % find the most recent registration
+    registrationIndex=find((cur.time(1)-regTimes)>0,1,'last');
     regFolder=regNames{registrationIndex};
+    
+    % Get ground truth points
     [arteryInRobot,HOrgan] = getArteryPoints([dataFolder filesep regFolder],organLabel,featureFolder);
-    
-    figure
-    vplot3(cur.pos/1000)
-    hold on
-    vplot3(arteryInRobot)
-    
-    % Micron data is reported in organ frame, which is weird, but ok
+    organInRobot = getOrganPoints([dataFolder filesep regFolder],organLabel,organFolder);
+
+    %% Get micron data
+    % Transform Micron data from organ frame to robot frame
+    % (it's in organ frame for historical reasons, don't worry about why)
     registrationFilePath = [dataFolder filesep regFolder filesep 'Micron2Phantom' num2str(label2num(organLabel)) '.txt'];
     HMicron=readTxtReg(registrationFilePath);
     micronHomog=HOrgan*(HMicron\[micronTip.pos';ones(1,length(micronTip.pos))]);
-    vplot3(micronHomog(1:3,:)');
     
-    % Add time processing for when each is started/finished
-%     USE SIGNALS FROM FOOTPEDAL! MAKE SURE TO RECORD/EXTRACT THOSE!
+    % Process data for when each piece of the experiment is started/finished
+    startTime = output.buttons.coag.time(1);
+    cameraTimes=output.buttons.camera.time(output.buttons.camera.push==1);
+    timeTrim=[startTime;repelem(cameraTimes,2)];
     
-    %QUESTION: when evaluating distance from line, do we project onto the
-    %organ/the local plane of the artery? 3D errors would correlate with
-    %force errors, which isn't really what we want to do. I think the
-    %wisest thing is to project onto the closest surface point during curve
-    %following? But then there's also the question of "amount in contact"
-%     THIS IS A QUESTION FOR A LATER DATE, WE HAVE ALL THE RELEVANT
-%     INFORMATION RIGHT NOW
+    %     What happens if there are more than 3 camera presses? That will break
+    %     the VF, so should be ok, just restart the experiment, have to throw
+    %     it out anyway
+    [micronTrim,trimmedTime]=trimBetweenTime(micronHomog(1:3,:)',output.micronTip.time,timeTrim,true);
+    curTrim=trimBetweenTime(cur.pos/1000,cur.time,timeTrim,true);
+    curTrimContact=trimBetweenTime(curContact,curTimes,timeTrim,true);
+    
+    %% TEMP PLOTTING, REMOVE AFTER TESTING
+%     TODO do an experiment with intentional liftoff during following to
+%     see the result and make sure this is captured
+%     for jj=1:3
+%         figure
+%         vplot3(curTrim{jj});
+%         hold on
+%         vplot3(curTrimContact{jj});
+%     end
 
+    %% Find distance from the line
+    % Metric 1: Lateral error and force tracking while in contact:
+    % If the user skips a portion of the curve with no contact, this error metric would not see that.
 
-% HOW DO WE DEAL WITH NO-CONTACT SECTIONS? IF IT'S FOR READJUSTMENT, THAT
-% WOULD BE FINE, BUT IF ITS BECAUSE OF COMPLETE LACK OF FOLLOWING, THAT
-% WOULD BE BAD. THERE'S A QUESTION OF INTENT WHICH IS DIFFICULT TO PARSE
-% Option A: Use all data always. This means that any readjustment would
-% be counted as errors in tracking. Also use all force data, so
-% readjustment errors would also show up as force errors.
-% Option B: Use data only when in contact. This means that if the
-% user skips a portion of the curve with no contact, their error metric
-% would not see that. Simultaneously, if we only use in-contact forces, the
-% force metric would not pick up 'missing' sections of the organ either.
-% **Option C: Option B but add a "coverage" metric that looks at how much of
-% the curve is "covered" (closest point) during curve-following *during*
-% contact. This way if they leave the organ, we will see a lack of coverage
-% Users may not leave the surface much, leaving this moot. But it's
-% important to think about upfront
-
-    % Add the organ itself
-    organInRobot = getOrganPoints([dataFolder filesep regFolder],organLabel,organFolder);
-    vplot3(organInRobot')
-
-    figure
+    % Do we want project errors or 3D Errors?
+    % 3D errors would correlate with force errors, correlates metrics
+    [arteryPlane.n,arteryPlane.p]=fitPlane(arteryInRobot);
+    
+    for jj=1:3
+        curProj{jj}=proj_onto_a_plane(arteryPlane,curTrim{jj});
+        [D{jj},I{jj}]=pdist2(arteryInRobot,curTrim{jj},'euclidean','Smallest',1);
+        dProj{jj}=pdist2(arteryInRobot,curProj{jj},'euclidean','Smallest',1);
+        vplot3(curTrim{jj},'.')
+        
+        hold on
+        move=arteryInRobot(I{jj},:)-curTrim{jj};
+        plotVec(move',curTrim{jj}','k--')
+        meanDistError(jj)=mean(D{jj});
+        meanProjDist(jj)=mean(dProj{jj});
+    end
+    vplot3(arteryInRobot)
+    plotPlane(arteryPlane.n,arteryPlane.p,0.04)
+    
+    % Metric 2: how much of the curve is "covered" (closest point) *during* contact.
+    % If the user leaves the organ, we will see a lack of coverage (or if
+    % they are super far away and miss sections of the artery... will have to look at that if it comes up)
+    for jj=1:3
+%         plot(unique(sort(I{jj})))
+        coverage(jj)=length(unique(I{jj}))/length(arteryInRobot);
+    end
+    
+    %% Find force folowing errors
     forceNorm=sqrt(sum(force.data.^2,2));
-    plot(forceNorm)
+    %Don't include when not in contact, don't need to follow forces then
+    contactforceNorm=trimBetweenTime(forceNorm,force.time,contactTrimTimes); 
+    
+    %% Plot results
+    if plotOption
+        % Plot the in contact and free space data
+        figure
+        vplot3(curContact)
+        hold on
+        vplot3(arteryInRobot)
+        vplot3(micronHomog(1:3,:)');
+        vplot3(organInRobot')
+        
+        % Plot the force norm data
+        figure
+        
+        plot(forceNorm)
+    end
 end
 
 %%
 % Read in organ, get ground truth location of organ/spheres
-for ii=5%4:5
-    organLabel=expOrgan{ii};
-    figure
-    % Read data file
-    output=readRobTxt(dataFolder,expName{ii});
-    cur=output.psm_cur;
-    micronTip=output.micronTip;
-    
-    % Find the most recent registration
-    registrationIndex=find((cur.time(1)-regTimes)>0,1,'last'); 
-    regFolder=regNames{registrationIndex};
+for ii=5:6
+    for jj=1:length(expOrgan{ii})
+        organLabel=expOrgan{ii}{jj};
+        figure
+        % Read data file
+        output=readRobTxt(dataFolder,expName{ii}{jj});
+        cur=output.psm_cur;
+        micronTip=output.micronTip;
 
-	% Get Sphere Points
-    [spheresInRobot,H1,sphereRaw] = getSpherePoints([dataFolder filesep regFolder],organLabel,featureFolder);
-    
-    % Get times closest to selected POI times
-    t1 = output.poi_points.time;
-    t2 = output.psm_cur.time;
-    [~, idxB] = min( abs(t1(:)-t2(:)') ,[],2);
-    savedPoints=output.psm_cur.pos(idxB,:)/1000;
-    
-    % Get Organ Points
-    [organInRobot,H2,organRaw] = getOrganPoints([dataFolder filesep regFolder],organLabel,organFolder);
-    
-    % Plot results
-    vplot3(organInRobot')
-    hold on
-    vplot3(spheresInRobot','o')
-    vplot3(savedPoints,'x')
-    
-    registrationFilePath = [dataFolder filesep regFolder filesep 'Micron2Phantom' num2str(label2num(organLabel)) '.txt'];
-    HMicron=readTxtReg(registrationFilePath);
-    micronHomog=H2*(HMicron\[micronTip.pos';ones(1,length(micronTip.pos))]);
-    vplot3(micronHomog(1:3,:)');    
-    
-%     TODO: need to convert robot selected points to closest on
-%     organ and then distance to the sphere center (which I think we've
-%     maybe already converted to closest mesh points)
-    
-% Metric question: How to represent error in selection?
-%     average distace from selected spot to hard nodule?
-%     average distance from hard nodule to closest selection?
+        % Find the most recent registration
+        registrationIndex=find((cur.time(1)-regTimes)>0,1,'last');
+        regFolder=regNames{registrationIndex};
 
-% Metric question: how do we count that a nodule has been found?
-%     Define some arbitrary epsilon that indicates a nodule has been
-%     "located"
-    
-    % Total Experiment time in seconds (convert from nanoseconds)
-    experimentLengthInS = (output.psm_cur.time(end)-output.psm_cur.time(1))/1E9;
+        % Get Sphere Points
+        [spheresInRobot,H1,sphereRaw] = getSpherePoints([dataFolder filesep regFolder],organLabel,featureFolder);
+
+%% Find actual button times
+t=[output.buttons.camera.time];
+t=t(logical([output.buttons.camera.push]));
+tstart=t(1);
+tend=t(2);
+
+tSearch = output.poi_points.time;   
+test=tSearch(tSearch>tstart & tSearch<tend); %only look in between first and second cam button
+% 
+plusTime=[output.buttons.camplus.time];
+plusButton=[output.buttons.camplus.push];
+plusTime=plusTime(logical(plusButton));
+plusTime=plusTime(plusTime>tstart & plusTime<tend); % Trim plus times
+
+
+%
+minusTime=[output.buttons.camminus.time];
+minusButton=[output.buttons.camminus.push];
+minusTime=minusTime(logical(minusButton));
+minusTime=minusTime(minusTime>tstart & minusTime<tend); % Trim minus times
+
+
+        % Get times closest to selected POI times
+        t1 = output.poi_points.time;
+        t2 = output.psm_cur.time;
+        [~, idxB] = min( abs(t1(:)-t2(:)') ,[],2);
+        savedPoints=output.psm_cur.pos(idxB,:)/1000;
+
+        % Get Organ Points
+        [organInRobot,H2,organRaw] = getOrganPoints([dataFolder filesep regFolder],organLabel,organFolder);
+
+        % Plot results
+        vplot3(organInRobot')
+        hold on
+        vplot3(spheresInRobot','o')
+        vplot3(savedPoints,'x')
+
+        registrationFilePath = [dataFolder filesep regFolder filesep 'Micron2Phantom' num2str(label2num(organLabel)) '.txt'];
+        HMicron=readTxtReg(registrationFilePath);
+        micronHomog=H2*(HMicron\[micronTip.pos';ones(1,length(micronTip.pos))]);
+        vplot3(micronHomog(1:3,:)');
+
+        %     TODO: need to convert robot selected points to closest on
+        %     organ and then distance to the sphere center (which I think we've
+        %     maybe already converted to closest mesh points)
+
+        % Metric question: How to represent error in selection?
+        %     average distace from selected spot to hard nodule?
+        %     average distance from hard nodule to closest selection?
+
+        % Metric question: how do we count that a nodule has been found?
+        %     Define some arbitrary epsilon that indicates a nodule has been
+        %     "located"
+
+        % Total Experiment time in seconds (convert from nanoseconds)
+        experimentLengthInS = (output.psm_cur.time(end)-output.psm_cur.time(1))/1E9;
+    end
 end
--- scripts/Utilities/trimBetweenTime.m
+++ scripts/Utilities/trimBetweenTime.m
@@ -1,7 +1,18 @@
-function data_trimmed=trimBetweenTime(data,timeFull,trimTime)
+function [data_trimmed,time_trimmed]=trimBetweenTime(data,timeFull,trimTime,isCell)
+if nargin<4
+    isCell=false;
+end
 data_trimmed=[];
+time_trimmed=[];
 for i=1:2:length(trimTime)-1
-    data_trimmed=[data_trimmed;data(timeFull>=trimTime(i) & timeFull<=trimTime(i+1),:)];
+    timeIndex=timeFull>=trimTime(i) & timeFull<=trimTime(i+1);
+    if isCell
+        data_trimmed{(i+1)/2}=data(timeIndex,:);
+        time_trimmed{(i+1)/2}=timeFull(timeIndex,:);
+    else
+        data_trimmed=[data_trimmed;data(timeIndex,:)];
+        time_trimmed=[time_trimmed;timeFull(timeIndex,:)];
+    end
 end
 
 end
